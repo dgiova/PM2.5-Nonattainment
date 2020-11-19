6d9edb63b50fa408c133e33b24dc6359014e21fb
Davide Giovanardi <davide3@stanford.edu>
2020-11-18 22:21:20 -0800

    clean code

--- MSE327/DIM.R
+++ MSE327/DIM.R
@@ -1,4 +1,3 @@
-
 compute_var <- function(Z, N, y) {
   y_bar <- sum(Z * y) / N
   var_Z <- 1/(N-1) * sum((Z * (y-y_bar)^2))
@@ -6,29 +5,23 @@ compute_var <- function(Z, N, y) {
 }
 
 dat <- subset(fulldat, outofrange==0)
-cuts <- quantile(dat$ps, probs=c(0., .25, .5, .75, 1.))
-num_cat <- length(cuts)
-df <- na.omit(dat[,c("a","pmfu","ps","pscat")])
-df <- data.frame(df)
+df <- data.frame(na.omit(dat[,c("a","pmfu","pscat")]))
 
-strata_dim <- rep(0,num_cat-1)
-strata_var <- rep(0,num_cat-1)
-strata_weight <- rep(0,num_cat-1)
-
-# ALTERNATIVE METHOD commented out - replace with first two lines of for loop to use
-# for (i in 1:(num_cat-1)) {
-# z_strata <- df[df$ps >= cuts[i] & df$ps < cuts[i+1], c("a", "pmfu")]
+num_cat <- 4
+strata_dim <- rep(0,num_cat)
+strata_var <- rep(0,num_cat)
+strata_weight <- rep(0,num_cat)
 
 idx <- 1
-for (i in levels(df$pscat)) {
-  z_strata <- df[df$pscat == i, c("a", "pmfu")]
+for (cat in levels(df$pscat)) {
+  z_strata <- df[df$pscat == cat, c("a", "pmfu")]
   N_1 <- sum(z_strata[,"a"])
   N_0 <- nrow(z_strata) - N_1
   Z <- z_strata[,"a"]
   y <- z_strata[,"pmfu"]
   
   strata_dim[idx] <- (1/N_1 * sum(Z * y) -
-                      1/N_0 * sum((1 - Z) * y))
+                        1/N_0 * sum((1 - Z) * y))
   
   V_1 <- compute_var(Z, N_1, y)
   V_0 <- compute_var(1-Z, N_0, y)
